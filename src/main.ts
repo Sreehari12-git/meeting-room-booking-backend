9366ad2f9b19abda0fe9665e233db53cdbb19742
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-06-04 17:23:19 +0530

    Updated backend

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+  const allowedOrigins = process.env.ALLOWORIGINLIST ?? true ;
 
-  app.enableCors({
-    origin: 'https://meeting-room-booking-frontend.onrender.com',
-    credentials: true
-  })
+    app.enableCors({
+      origin: allowedOrigins,
+      credentials: true,
+    });
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
