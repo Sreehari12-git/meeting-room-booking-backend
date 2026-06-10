60e72fcd5f6197253f273e27d2cdde69cd659a61
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-06-10 18:33:00 +0530

    Fixed returning

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -144,6 +144,7 @@ export class BookingService {
             }
         })
         this.logger.info({ count: bookings.length }, "All bookings fetched")
+        return bookings;
     }
 
     async cancelBooking(bookingId: number,userId: number) {
