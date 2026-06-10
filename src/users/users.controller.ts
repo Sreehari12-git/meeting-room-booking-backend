d9472116884216c2f75dc5f287fa3d87aa361f8f
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-06-10 11:41:49 +0530

    Converted the server time to localtime

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,24 +6,28 @@ import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { Role } from '@prisma/client';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { error } from 'console';
 
 @Controller("admin") @UseGuards(RolesGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @ApiOperation({summary: "Used to create new product"})
-  @ApiResponse({status: 201, description: "User created"})
+  @ApiOperation({summary: "Used to create new user"})
+  @ApiResponse({status: 201, description: "User created successfully",schema: {example: {message: "User created successfully", user: {id: 1, name: "John Doe", email: "john@example.com", role: "USER"}}}})
   @ApiBearerAuth()
   @Post("create-user") @Roles(Role.ADMIN)
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.createUser(body);
   }
 
+  @ApiOperation({summary: "Used to create admin"})
+  @ApiResponse({status: 201, description: "Admin created successfully", schema: {example: {message: "Admin created successfully", user: {id: 1, name: "Admin", email: "admin@gmail.com", role: "ADMIN"}}}})
   @Post("create-admin")
   createAdmin(@Body() body: CreateUserDto) {
     return this.usersService.createAdmin(body);
   }
 
+  @ApiResponse({status: 200, description: "Users fetched successfully", schema: {example: {message: "Users fetched successfully", users: [{id: 1, name: "John Doe", email: "john@example.com", role: "USER"}]}}})
   @Get("get-all")
   @ApiOperation({ summary: 'Get all users' })
   @ApiResponse({ status: 200, description: 'Users fetched successfully' })
@@ -34,7 +38,7 @@ export class UsersController {
 
   @Delete("user/:email") @Roles(Role.ADMIN)
   @ApiOperation({ summary: 'Delete user by email' })
-  @ApiResponse({ status: 200, description: 'User deleted successfully' })
+  @ApiResponse({ status: 200, description: 'User deleted successfully', schema: {example: {message: "User deleted succesfully"}} })
   @ApiResponse({ status: 404, description: 'User not found' })
   @ApiBearerAuth()
   deleteUser(@Param("email") email: string) {
@@ -42,8 +46,8 @@ export class UsersController {
   }
 
   @ApiOperation({ summary: 'Update user details' })
-  @ApiResponse({ status: 200, description: 'User updated successfully' })
-  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 200, description: 'User updated successfully', schema: {example: {message: "User updated successfully", user: {id : 1, name: "John Doe", email: "john@example.com", role: "USER" }}} })
+  @ApiResponse({ status: 404, description: 'User not found', schema: {example: {statusCode: 404, message: "User not found", error: "Not found"}} })
   @ApiBearerAuth()
   @Put("user/:email") @Roles(Role.ADMIN)
   updateUser(@Param("email") email: string,@Body() body: any) {
